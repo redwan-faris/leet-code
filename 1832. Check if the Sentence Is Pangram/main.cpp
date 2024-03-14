43ba6627f8468eaf916c1c059e1839454e71a436
redwan-faris <redwanfarisa@gmail.com>
2024-03-14 21:42:14 +0300

    feat(solve) : 1832. Check if the Sentence Is Pangram

---
+++ 1832. Check if the Sentence Is Pangram/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool checkIfPangram(string sentence)
+{
+    set<char> alpha;
+    for (int i = 0; i < sentence.size(); i++)
+    {
+        alpha.insert(sentence[i]);
+    }
+    if (alpha.size() == 26)
+        return true;
+    return false;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        string sentence;
+        cin >> sentence;
+        cout << checkIfPangram(sentence);
+    }
+    return 0;
+}

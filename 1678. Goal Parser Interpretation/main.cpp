e2035a8e3c7d1e1ce76643c51c107f8e7b213315
redwan-faris <redwanfarisa@gmail.com>
2024-03-14 21:38:01 +0300

    feat(solve) : 1678. Goal Parser Interpretation

---
+++ 1678. Goal Parser Interpretation/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+string interpret(string command)
+{
+    string result;
+    for (int i = 0; i < command.size() - 1; i++)
+    {
+        if (command[i] == '(')
+        {
+            if (command[i + 1] == ')')
+            {
+                result.push_back('o');
+                i++;
+            }
+            else
+            {
+                result.push_back('a');
+                result.push_back('l');
+                i = i + 2;
+            }
+        }
+        else
+        {
+            result.push_back('G');
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        string command;
+        cin >> command;
+        cout << interpret(command);
+    }
+    return 0;
+}
